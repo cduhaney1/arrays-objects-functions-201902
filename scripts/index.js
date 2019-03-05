7bee98915fe0b94543b69a9d09b38e535819db98
chastity duhaney <chastity_duhaney82@gmail.com>
2019-03-05 15:13:54 -0500

    First excercise

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
-// add your solutions here
+function isPositive(value) {
+    return value > 0;
+  }
+  
+  let filtered = [5, -2, 4, -1, 10].filter(isPositive);
+  console.log(filtered);
+// Write a function which takes an array of numbers as input and returns a new array containing only the positive numbers in the given array.
